733ceb7046ba6767b5051c030e666f8a8d1e31e5
Peter <peterkinmond@gmail.com>
2012-11-10 11:45:21 -0800

    Cookie remembers which level user achieved.
    * Add 5 mazes of increasing difficulty

--- demos/maze/maze_properties.js
+++ demos/maze/maze_properties.js
@@ -15,36 +15,107 @@ Mazes = [
   Maze = {
     ID: 1,
     MAP: [
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 2, 1, 1, 1, 1, 3, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0]],
+      [0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 2, 1, 1, 1, 1, 3, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0]],
     TITLE: 'Maze 1: Go, Go, Go!',
     DESCRIPTION: 'The fastest way from point A to point B is a straight line.'
   },
+
   Maze = {
     ID: 2,
     MAP: [
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 2, 1, 1, 1, 1, 3, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0]],
-    TITLE: 'MAZE 2!!!!',
-    DESCRIPTION: 'This is the SECOND MAZE'
-  }
+      [0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 3, 0, 0, 0],
+      [0, 0, 0, 0, 1, 0, 0, 0],
+      [0, 0, 0, 0, 1, 0, 0, 0],
+      [0, 2, 1, 1, 1, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0]],
+    TITLE: 'Maze 2: Hang a left',
+    DESCRIPTION: ''
+  },
+
+  Maze = {
+    ID: 3,
+    MAP: [
+      [0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 1, 1, 1, 3, 0],
+      [0, 0, 0, 1, 0, 0, 0, 0],
+      [0, 0, 0, 1, 0, 0, 0, 0],
+      [0, 2, 1, 1, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0]],
+    TITLE: 'Maze 3: Zig Zag',
+    DESCRIPTION: 'To the window, to the wall..'
+  },
+
+  Maze = {
+    ID: 4,
+    MAP: [
+      [0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 1, 1, 1, 3, 0],
+      [0, 0, 0, 1, 0, 0, 0, 0],
+      [0, 0, 0, 1, 0, 0, 0, 0],
+      [0, 2, 1, 1, 0, 0, 0, 0],
+      [0, 0, 0, 1, 0, 0, 0, 0],
+      [0, 0, 0, 1, 1, 1, 1, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0]],
+    TITLE: 'Maze 4: The Road Less Traveled',
+    DESCRIPTION: 'Two roads diverged in a wood, and I— <br/>\
+      I took the one less traveled by,<br/> \
+      And that has made all the difference.'
+  },
+
+  Maze = {
+    ID: 5,
+    MAP: [
+      [0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 1, 1, 0, 1, 0, 3, 0],
+      [0, 1, 1, 0, 1, 1, 1, 0],
+      [0, 1, 0, 0, 1, 0, 0, 0],
+      [0, 1, 1, 1, 1, 1, 1, 0],
+      [0, 0, 1, 0, 0, 0, 1, 0],
+      [0, 2, 1, 1, 1, 0, 1, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0]],
+    TITLE: 'Maze 5: The Unpossible Dream',
+    DESCRIPTION: 'This is my quest, to follow that star<br/> \
+      No matter how hopeless, no matter how far'
+  },
 ];
 
-
-Maze = Mazes[0];
+var mazeNumber = parseInt(getCookie('BlocklyMazeNumber')) || 1;
+Maze = Mazes[mazeNumber - 1];
 
 Maze.next = function() {
-  Maze = Mazes[1];
+  setCookie('BlocklyMazeNumber', mazeNumber + 1);
+  location.href='maze.html';
 };
+
+function setCookie(key, value) {
+  var today = new Date();
+  var expiry = new Date(today.getTime() + 30 * 24 * 3600 * 1000); // plus 30 days
+  document.cookie = key + "=" + value + "; path=/; expires=" + expiry.toGMTString();
+}
+
+function getCookie(c_name)
+{
+  var i,x,y,ARRcookies=document.cookie.split(";");
+  for (i=0;i<ARRcookies.length;i++)
+  {
+    x=ARRcookies[i].substr(0,ARRcookies[i].indexOf("="));
+    y=ARRcookies[i].substr(ARRcookies[i].indexOf("=")+1);
+    x=x.replace(/^\s+|\s+$/g,"");
+    if (x==c_name)
+    {
+    return unescape(y);
+    }
+  }
+}
